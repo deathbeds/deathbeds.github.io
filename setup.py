97d957d3efae74a96ae3d5d9005cb91aded92026
tonyfast <tony.fast@gmail.com>
2018-07-02 18:09:37 -0400

    Move notebooks to deathbeds so it can be imported

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 __import__('setuptools').setup(
     name="deathbeds",
-    version="0.0.1",
-    author="deathbeds", author_email="tony.fast@gmail.com",
+    version="0.0.2",
+    author="deathbeds", 
+    author_email="tony.fast@gmail.com",
     description="sand to sand, silicon to silicon", 
     license="BSD-3-Clause",
     packages=['deathbeds'],
