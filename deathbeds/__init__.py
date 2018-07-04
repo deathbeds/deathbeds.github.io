ccada2d1e11d89158301cd7c95cfdb71f9c993f0
tonyfast <tony.fast@gmail.com>
2018-07-03 23:28:19 -0400

    Update init

--- deathbeds/__init__.py
+++
@@ -1,5 +0,0 @@
-from aye import Import, update_hooks, Notebook
-update_hooks(Notebook)
-
-from pathlib import Path
-notebooks = {path.with_suffix('').name: path for path in Path(__file__).parent.glob('*.ipynb')}
