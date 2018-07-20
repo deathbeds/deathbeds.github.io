005f56bdf41f4c78a7dd25b93388d07f11696998
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-07-20 18:04:32 -0400

    use source

--- .ci/travis_install.sh
+++ .ci/travis_install.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -ex
-$CONDA_DIR/bin/conda activate base
+
+source $CONDA_DIR/bin/activate base
+
 python setup.py sdist
 python setup.py bdist_wheel
+
 conda env update -n root -f environment.yml
+
 python -m pip install -r requirements.txt
+
 python -m ipython profile create default
+
 importnb-install
+
 python -c "__import__('importnb.utils.ipython').utils.ipython.install('pidgin')"
+
 python -m pip install dist/deathbeds-*.tar.gz --ignore-installed --no-deps
--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-$CONDA_DIR/bin/conda activate base
+
+source $CONDA_DIR/bin/activate base
 
 # on a branch only the notebook on the branch should run; on master everything.
 ls deathbeds/*.ipynb |
