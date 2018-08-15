6ddd3e601cfde292f3c3ef428a1da978fc2f9b58
Tony Fast <tony.fast@gmail.com>
2018-08-14 21:15:20 -0400

    update travis test

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -4,10 +4,10 @@ set -ex
 source $CONDA_DIR/bin/activate base
 
 # on a branch only the notebook on the branch should run; on master everything.
+ipython -m deathbeds.tests -- --verbose
+
 ls deathbeds/*.ipynb |
   xargs python -m jupyter nbconvert \
     --execute \
     --ExecutePreprocessor.kernel_name=python \
     --to notebook
-
-ipython -m deathbeds.tests -- --verbose
