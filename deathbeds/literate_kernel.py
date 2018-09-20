0a19f9e5b51990a8edd49fd2f46b4bb136a75edb
Tony Fast <tony.fast@gmail.com>
2018-09-19 23:04:19 -0400

    Add to the literate kernel

--- deathbeds/literate_kernel.py
+++ deathbeds/literate_kernel.py
@@ -1,6 +1,6 @@
 
 with __import__('importnb').Notebook():
-    from deathbeds.__Literate_Markdown_Kernel import LiterateKernel
+    from deathbeds.__Inspecting_Literate_Kernel import LiterateKernel
 
 if __name__ == '__main__':
     from ipykernel.kernelapp import IPKernelApp
