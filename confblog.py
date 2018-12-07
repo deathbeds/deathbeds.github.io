8cff6c7815f4f1fcfc3de47f111900abc7103670
Tony Fast <tony.fast@gmail.com>
2018-12-07 12:40:42 -0500

    Update confblog.py
    change theme

--- confblog.py
+++ confblog.py
@@ -150,7 +150,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap4"
+THEME = "hack"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
@@ -1375,8 +1375,9 @@ WARN_ABOUT_TAG_METADATA = False
 # Put in global_context things you want available on all your templates.
 # It can be anything, data, functions, modules, etc.
 GLOBAL_CONTEXT = {
-    'exclude_index_content': True
 }
+
+GLOBAL_CONTEXT['HACK_VARIANT'] = 'dark'
 
 # Add functions here and they will be called with template
 # GLOBAL_CONTEXT as parameter when the template is about to be
